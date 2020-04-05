0fed7851383536fc7f5980c88660c28975a7d0a9
dlyin <yindongliang0531@163.com>
2020-04-05 22:03:54 +0800

    初始化数据库，和构建项目脚本

--- maven/buildm-project.sh
+++ maven/buildm-project.sh
@@ -3,13 +3,18 @@
 cid=$(docker ps | grep my-maven |awk '{print $1}')
 
 build(){
-
- docker exec -w /home/myuser/projects -i $2 git clone https://github.com/snake4/$1
- docker exec -w /home/myuser/projects/$1 -i $2 mvn deploy -Dmaven.test.skip=true 
+ docker exec -w /home/myuser/projects -i $2 rm -rf $1
+ docker exec -w /home/myuser/projects -i $2 git clone -b 2.0.2 https://github.com/snake4/$1
+ docker exec -w /home/myuser/projects/$1 -i $2 mvn clean deploy -Dmaven.test.skip=true 
 }
 
 project=vt-c-projectUtils
 build $project $cid
+
+
+project=vt-c-redisson
+build $project $cid
+
 
 project=vt-c-shiroJpa
 build $project $cid
@@ -25,4 +30,5 @@ build $project $cid
 
 project=vt-c-dataSourceMulti
 build $project $cid
+
 
