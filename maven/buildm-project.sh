ff9462eba5dc7e2cfc4e1542ab15e2ee6f8b2959
dlyin <yindongliang0531@163.com>
2020-04-08 00:27:48 +0800

    pom release修改

--- maven/buildm-project.sh
+++ maven/buildm-project.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
+
+
 #container id
 cid=$(docker ps | grep my-maven |awk '{print $1}')
 
-build(){
+cloneAndDeploy(){
  docker exec -w /home/myuser/projects -i $2 rm -rf $1
  docker exec -w /home/myuser/projects -i $2 git clone -b 2.0.2 https://github.com/snake4/$1
  docker exec -w /home/myuser/projects/$1 -i $2 mvn clean deploy -Dmaven.test.skip=true 
 }
 
+cloneAndPackageAndStart(){
+ docker exec -w /home/myuser/projects -i $2 rm -rf $1
+ docker exec -w /home/myuser/projects -i $2 git clone -b 2.0.2 https://github.com/snake4/$1
+ docker exec -w /home/myuser/projects/$1 -i $2 mvn clean package -Dmaven.test.skip=true 
+ docker exec -w /home/myuser/projects/$1/target -i $2 nohup java -jar  $1-spring-boot-starter-1.0.jar >/dev/null 2>&1 &
+}
+if [ -n "$1" ]; then
+ project=$1
+ cloneAndDeploy $project $cid
+ exit 0
+fi
+
+
 project=vt-c-projectUtils
-build $project $cid
+cloneAndDeploy $project $cid
 
 
 project=vt-c-redisson
-build $project $cid
+cloneAndDeploy $project $cid
 
 
 project=vt-c-shiroJpa
-build $project $cid
+cloneAndDeploy $project $cid
 
 project=vt-c-logAndSecurityAndThreadPool
-build $project $cid
+cloneAndDeploy $project $cid
 
 project=vt-c-shiroClient
-build $project $cid
+cloneAndDeploy $project $cid
 
 project=vt-c-jxlss
-build $project $cid
+cloneAndDeploy $project $cid
 
 project=vt-c-dataSourceMulti
-build $project $cid
+cloneAndDeploy $project $cid
 
+project=vt-s-authenCenter
+cloneAndPackageAndStart $project $cid
 
+project=vt-test-project
+cloneAndPackageAndStart $project $cid
