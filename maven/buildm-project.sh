8e73d39815d8762fc2f6044fe9044e729e0d4e71
dlyin <yindongliang0531@163.com>
2020-04-08 00:30:22 +0800

    取消2.0.2分支

--- maven/buildm-project.sh
+++ maven/buildm-project.sh
@@ -6,13 +6,13 @@ cid=$(docker ps | grep my-maven |awk '{print $1}')
 
 cloneAndDeploy(){
  docker exec -w /home/myuser/projects -i $2 rm -rf $1
- docker exec -w /home/myuser/projects -i $2 git clone -b 2.0.2 https://github.com/snake4/$1
+ docker exec -w /home/myuser/projects -i $2 git clone  https://github.com/snake4/$1
  docker exec -w /home/myuser/projects/$1 -i $2 mvn clean deploy -Dmaven.test.skip=true 
 }
 
 cloneAndPackageAndStart(){
  docker exec -w /home/myuser/projects -i $2 rm -rf $1
- docker exec -w /home/myuser/projects -i $2 git clone -b 2.0.2 https://github.com/snake4/$1
+ docker exec -w /home/myuser/projects -i $2 git clone  https://github.com/snake4/$1
  docker exec -w /home/myuser/projects/$1 -i $2 mvn clean package -Dmaven.test.skip=true 
  docker exec -w /home/myuser/projects/$1/target -i $2 nohup java -jar  $1-spring-boot-starter-1.0.jar >/dev/null 2>&1 &
 }
